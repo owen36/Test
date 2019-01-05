2821c29abf5efe6b8ce655a9f5d0f877887f37ad
owen36 <mo1s@hotmail.co.uk>
2019-01-05 13:28:12 +0000

    added simple Icon functions

---
+++ Heist/Assets/Scripts/UI Scripts/Framework/Icons.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Icons : MonoBehaviour
+{
+    public Image selectionImage;
+    public Button selfButton;
+
+    private int clickCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        clickCount = 0;
+        selfButton.onClick.AddListener(OnClick);
+    }
+
+
+    private void OnClick()
+    {
+        clickCount++;
+
+        selectionImage.enabled = clickCount >= 1;
+
+        if(clickCount >= 2)
+        {
+            // open window here
+            selectionImage.enabled = false;
+            clickCount = 0;
+        }
+    }
+}
