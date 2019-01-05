824e8923183d22c9ab1f30f0332d27d5ece6aae9
owen36 <mo1s@hotmail.co.uk>
2019-01-05 14:24:07 +0000

    added date and time

---
+++ Heist/Assets/Scripts/UI Scripts/Framework/DateTime.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DateTime : MonoBehaviour
+{
+    public Text time;
+    public Text date;
+
+    private System.DateTime localTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        localTime = System.DateTime.Now;
+        time.text = localTime.ToShortTimeString();
+        date.text = localTime.ToShortDateString();
+    }
+}
