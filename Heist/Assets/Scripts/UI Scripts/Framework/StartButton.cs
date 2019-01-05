e4500d6d3b5555b5a5f378ff2d43f634de76e099
owen36 <mo1s@hotmail.co.uk>
2019-01-05 14:09:33 +0000

    added start menu functionality

---
+++ Heist/Assets/Scripts/UI Scripts/Framework/StartButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour
+{
+    public GameObject startMenu;
+    public Button startButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(OnClick);
+    }
+
+    private void OnClick()
+    {
+        if (startMenu.activeSelf)
+            startMenu.SetActive(false);
+        else
+            startMenu.SetActive(true);
+    }
+}
---
+++ Heist/Assets/Scripts/UI Scripts/Framework/StartMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+    // gonna add in all the buttons and functions here
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !IsMouseOverStartMenu())
+        {
+                Debug.Log("trigger");
+                this.gameObject.SetActive(false);    
+        }
+    }
+
+    private bool IsMouseOverStartMenu()
+    {
+        PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
+        pointerEventData.position = Input.mousePosition;
+
+        List<RaycastResult> raycastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointerEventData, raycastResults);
+
+        foreach (RaycastResult result in raycastResults)
+        {
+            if (result.gameObject.name == "Start Menu")
+                return true;
+        }
+
+        return false;
+    }
+}
